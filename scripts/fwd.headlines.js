86c9ab789456e5dd63cbcbc06ae78fe0422758cb
Luke Wright <luke@simplyluke.com>
2015-04-10 21:07:25 -0500

    #typed

---
+++ scripts/fwd.headlines.js
@@ -0,0 +1,38 @@
+$(function(){
+        $("#headline-target").typed({
+            strings: [
+						"Immigration is not a <strong>ZERO</strong> sum game", 
+						"Immigrants actually create jobs and we've got the <a href='http://www.aei.org/wp-content/uploads/2011/12/-immigration-and-american-jobs_144002688962.pdf' target='_stats'>stats to prove it</a>!", 
+						],
+            typeSpeed: 0, 
+			contentType: 'html'
+			/* // typing speed
+            typeSpeed: 0,
+            // time before typing starts
+            startDelay: 0,
+            // backspacing speed
+            backSpeed: 0,
+            // time before backspacing
+            backDelay: 500,
+            // loop
+            loop: false,
+            // false = infinite
+            loopCount: false,
+            // show cursor
+            showCursor: true,
+            // character for cursor
+            cursorChar: "|",
+            // attribute to type (null == text)
+            attr: null,
+            // either html or text
+            contentType: 'html',
+            // call when done callback function
+            callback: function() {},
+            // starting callback function before each string
+            preStringTyped: function() {},
+            //callback for every typed string
+            onStringTyped: function() {},
+            // callback for reset
+            resetCallback: function() {}*/
+        });
+});
